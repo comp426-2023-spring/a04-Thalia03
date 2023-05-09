0bde187b3aecbb158b429fc226814e1939f861d5
Thalia Rodriguez <thalia03@ad.unc.edu>
2023-05-09 16:36:51 -0400

    3

--- server.js
+++ server.js
@@ -10,15 +10,46 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.get('/app', (req, res) => {
-    res.status(200).send('200 OK');
+app.get('/app', (_req, res) => {
+    res.status(200).send('200 OK').end();
 })
 
 app.get('/app/rps', (req, res) => {
-    res.status(200).send(rps());
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
 })
 
 app.get('/app/rpsls', (req, res) => {
-    res.status(200).send(rpsls());
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
 })
+
+app.get('/app/rps/play', (req, res) => {
+    res.status(200).send(rps(req.query.shot));
+})
+
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.query.shot));
+})
+
+app.post('/app/rps/play', (req, res) => {
+    res.status(200).send(rps(req.body.shot));
+})
+
+app.post('/app/rpsls/play', (req, res) => {
+    res.status(200).send(rpsls(req.body.shot));
+})
+
+
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(rps(req.params.shot));
+})
+
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(rpsls(req.params.shot));
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND').end();
+  });
+
+app.listen(port);
 
