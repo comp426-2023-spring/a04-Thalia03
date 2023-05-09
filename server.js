6f30b495ef958f3de3d8fe7622d51b3579f89c6c
Thalia Rodriguez <thalia03@ad.unc.edu>
2023-05-09 16:28:35 -0400

    12

--- server.js
+++ server.js
@@ -3,10 +3,22 @@ import minimist from 'minimist'
 import express from 'express'
 
 const args = minimist(process.argv.slice(2));
-const port = args.port || 5000;
+const port = args.port || args.p || 5000;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+app.get('/app', (req, res) => {
+    res.status(200).send('200 OK');
+})
+
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(rps());
+})
+
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(rpsls());
+})
 
